af40dea5636e43be97827e23236a895c2585e0a4
Kevin Gary <kevgary@Kevins-MacBook-Pro.local>
2016-04-10 02:39:11 -0600

    began host site

--- app.js
+++ app.js
@@ -1,102 +1,107 @@
-var boxes = {
+(function() {
 
-	events: {
-		'on .jiggy-box' : 'handleZipCodeLatLng'
-	},
+	var marginTop = ($('.jiggy-boxes').height() / $('.jiggy-boxes').width()) * 100 / 2;
 
-	initialize: function(config) {
-		var marginTop = ($('.jiggy-boxes').height() / $('.jiggy-boxes').width()) * 100 / 2;
+	$('.jiggy-box').on('click', function() {
+		$(this).focus();
+		
+		$('.jiggy-box').css({
+			'z-index': 0
+		});
 
-		$('.jiggy-box').focus();
-
-		$('.jiggy-box').on('keydown.ctrl_q', function(e) {
+		$(this).on('keydown.ctrl_q', function(e) {
 			console.log(e)
-			$('.jiggy-box').css({
+			$(this).css({
 				'margin-left': '0',
 				'margin-top': '0',
 				'width': '50%',
-				'height': '50%'
+				'height': '50%',
+				'z-index': 1
 			})
 		});
 
-		$('.jiggy-box').on('keydown.ctrl_w', function(e) {
+		$(this).on('keydown.ctrl_w', function(e) {
 			console.log(e)
-			$('.jiggy-box').css({
+			$(this).css({
 				'margin-left': '0',
 				'margin-top': '0',
 				'width': '100%',
-				'height': '50%'
+				'height': '50%',
+				'z-index': 1
 			})
 		});
 
-		$('.jiggy-box').on('keydown.ctrl_e', function(e) {
+		$(this).on('keydown.ctrl_e', function(e) {
 			console.log(e)
-			$('.jiggy-box').css({
+			$(this).css({
 				'margin-left': '50%',
 				'margin-top': '0',
 				'width': '50%',
-				'height': '50%'
+				'height': '50%',
+				'z-index': 1
 			})
 		});
-		$('.jiggy-box').on('keydown.ctrl_a', function(e) {
+		$(this).on('keydown.ctrl_a', function(e) {
 			console.log(e)
-			$('.jiggy-box').css({
+			$(this).css({
 				'margin-left': '0',
 				'margin-top': '0',
 				'width': '50%',
 				'height': '100%',
+				'z-index': 1
 			})
 		});
-		$('.jiggy-box').on('keydown.ctrl_s', function(e) {
+		$(this).on('keydown.ctrl_s', function(e) {
 			console.log(e)
-			$('.jiggy-box').css({
+			$(this).css({
 				'margin-left': '0',
 				'margin-top': '0',
 				'width': '100%',
 				'height': '100%',
+				'z-index': 1
 			})
 		});
-		$('.jiggy-box').on('keydown.ctrl_d', function(e) {
+		$(this).on('keydown.ctrl_d', function(e) {
 			console.log(e)
-			$('.jiggy-box').css({
+			$(this).css({
 				'margin-left': '50%',
 				'margin-top': '0',
 				'width': '50%',
 				'height': '100%',
+				'z-index': 1
 			})
 		});
-		$('.jiggy-box').on('keydown.ctrl_z', function(e) {
+		$(this).on('keydown.ctrl_z', function(e) {
 			console.log(e)
-			$('.jiggy-box').css({
+			$(this).css({
 				'margin-left': '0',
 				'margin-top': String(marginTop + '%'),
 				'width': '50%',
 				'height': '50%',
+				'z-index': 1
 			})
 		});
-		$('.jiggy-box').on('keydown.ctrl_x', function(e) {
+		$(this).on('keydown.ctrl_x', function(e) {
 			console.log(e)
-			$('.jiggy-box').css({
+			$(this).css({
 				'margin-left': '0',
 				'margin-top': String(marginTop + '%'),
 				'width': '100%',
 				'height': '50%',
+				'z-index': 1
 			})
 		});
-		$('.jiggy-box').on('keydown.ctrl_c', function(e) {
+		$(this).on('keydown.ctrl_c', function(e) {
 			console.log(e)
-			$('.jiggy-box').css({
+			$(this).css({
 				'margin-left': '50%',
 				'margin-top': String(marginTop + '%'),
 				'width': '50%',
 				'height': '50%',
+				'z-index': 1
 			})
 		});
+	});
 
 
-	}
-}
-
-
-
-boxes.initialize();
+})();
